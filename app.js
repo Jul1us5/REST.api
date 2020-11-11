3cc09f2949a6645f50ebb6f9531e49d54ea44a39
Julius <julius.vlasovas@gmail.com>
2020-11-11 17:42:24 +0200

    ...

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
-app.get('/api', (req, res) => {
-    res.json({
-        success: 1,
-        message: 'This is working!'
-    })
-})
+// app.get('/api', (req, res) => {
+//     res.json({
+//         success: 1,
+//         message: 'This is working!'
+//     })
+// })
 
 app.listen(process.env.APP_PORT, () => {
     console.log('Server is runing on : ', process.env.APP_PORT);
